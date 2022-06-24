f830e624b125b243dec020648da63ec9c072f387
MMDShen <80104926+MMDShen@users.noreply.github.com>
2022-06-24 11:31:56 +0430

    Update require func example.php

--- require func example.php
+++ require func example.php
@@ -1,4 +1,5 @@
 /* https://github.com/MMDShen/maktabSharif/blob/2e099178b0bc5b0f12c63b26a20f4a20c95551ba/callus.php  an example */
 /* https://github.com/MMDShen/maktabSharif/tree/HW3/callus-extentions its two parts */
 
-/* require if terminates with falsehood then next lines of code terminates as well with an error while include() just returns a warning and continue.. */
+/* require if terminates with falsehood then next lines of codes terminates as well with an error, while include() just returns a warning and continue.. */
+/*thx maktabsharif :  https://maktabsharif.ir/  */
